77be546b482d834c03bf94d6c3337a33bb50b00b
henrysun9074 <hs325@duke.edu>
2024-07-06 16:46:39 -0400

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   output: "server",
   adapter: netlify()
 } // output: 'server',
