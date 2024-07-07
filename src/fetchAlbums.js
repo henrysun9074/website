21202c3a86b9fa510046e9d5607435bfac7ee277
henrysun9074 <hs325@duke.edu>
2024-07-07 08:59:24 -0400

    Music page built

--- src/fetchAlbums.js
+++ src/fetchAlbums.js
@@ -1,35 +1,37 @@
-// src/fetchAlbums.js
-const fetch = require('node-fetch');
+// will implement later
 
-async function getSpotifyAccessToken() {
-  const clientId = '6457eb332a3e45a4b52f4f0db0db0b47';
-  const clientSecret = '8062a3d10df04572a7be217c65899b9f';
-  const authString = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+// // src/fetchAlbums.js
+// const fetch = require('node-fetch');
 
-  const response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Basic ${authString}`,
-    },
-    body: 'grant_type=client_credentials',
-  });
+// async function getSpotifyAccessToken() {
+//   const clientId = '6457eb332a3e45a4b52f4f0db0db0b47';
+//   const clientSecret = '8062a3d10df04572a7be217c65899b9f';
+//   const authString = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
-  const data = await response.json();
-  return data.access_token;
-}
+//   const response = await fetch('https://accounts.spotify.com/api/token', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//       'Authorization': `Basic ${authString}`,
+//     },
+//     body: 'grant_type=client_credentials',
+//   });
 
-async function fetchAlbums() {
-  const accessToken = await getSpotifyAccessToken();
+//   const data = await response.json();
+//   return data.access_token;
+// }
 
-  const response = await fetch('https://api.spotify.com/v1/me/albums', {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-    },
-  });
+// async function fetchAlbums() {
+//   const accessToken = await getSpotifyAccessToken();
 
-  const data = await response.json();
-  return data.items.map(item => item.album);
-}
+//   const response = await fetch('https://api.spotify.com/v1/me/albums', {
+//     headers: {
+//       'Authorization': `Bearer ${accessToken}`,
+//     },
+//   });
 
-module.exports = fetchAlbums;
+//   const data = await response.json();
+//   return data.items.map(item => item.album);
+// }
+
+// module.exports = fetchAlbums;
